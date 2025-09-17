ac0509bbc47ce596505fad8f83549bb8249bd079
DevDad123 <anhnq.b23dt013@stu.ptit.edu.vn>
2025-09-17 23:04:27 +0700

    Showcase Profile Page

--- lib/feature/profile/presentation/pages/profile_page.dart
+++ lib/feature/profile/presentation/pages/profile_page.dart
@@ -17,54 +17,154 @@ class _ProfilePageState extends State<ProfilePage> {
       backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
-        leading: Icon(Icons.arrow_back_ios, color: Colors.grey, size: 24),
-        actions: [
+        leading: const Icon(Icons.arrow_back_ios, color: Colors.grey, size: 24),
+        actions: const [
           Padding(
-            padding: const EdgeInsets.only(right: 13),
+            padding: EdgeInsets.only(right: 13),
             child: Icon(Icons.password, color: Colors.grey, size: 24),
           ),
         ],
       ),
       extendBodyBehindAppBar: true,
-      body: Container(
-        width: double.infinity,
-        height: size.height * 0.6,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/bgtheme.png"),
-            fit: BoxFit.cover,
-          ),
-        ),
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            CircleAvatar(
-              radius: 95,
-              backgroundColor: const Color.fromARGB(255, 118, 208, 121),
-              child: CircleAvatar(
-                radius: 90,
-                backgroundImage: AssetImage("assets/avatar.jpg.webp"),
-              ),
-            ),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+            Stack(
+              alignment: Alignment.center,
               children: [
-                Text(
-                  'Vladimir Putin',
-                  style: GoogleFonts.manrope(
-                    color: Colors.white,
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
+                Container(
+                  height: size.height * 0.5,
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("assets/bgtheme.png"),
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
-                SizedBox(width: 8,),
-                Icon(Icons.edit_outlined, color: Colors.grey,),
+                Column(
+                  children: [
+                    const SizedBox(height: 100),
+                    CircleAvatar(
+                      radius: 95,
+                      backgroundColor: const Color(0xFF1AAF74),
+                      child: const CircleAvatar(
+                        radius: 90,
+                        backgroundImage: AssetImage("assets/avatar.jpg.webp"),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Vladimir Putin',
+                          style: GoogleFonts.manrope(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        const SizedBox(width: 8),
+                        const Icon(Icons.edit_outlined, color: Colors.grey),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "064C818609",
+                          style: GoogleFonts.manrope(
+                            color: Colors.white,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        const SizedBox(width: 10),
+                        Container(
+                          height: 8,
+                          width: 8,
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color(0xFF1AAF74),
+                          ),
+                        ),
+                        const SizedBox(width: 5),
+                        Text(
+                          "Đang hoạt động",
+                          style: GoogleFonts.manrope(
+                            color: const Color(0xFF1AAF74),
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                  ],
+                ),
               ],
             ),
+            Container(
+              width: size.width * 0.95,
+              padding: const EdgeInsets.all(16),
+              margin: const EdgeInsets.only(top: 10),
+              decoration: BoxDecoration(
+                color: const Color(0xFF1A1D1F),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Column(
+                children: [
+                  infoRow("Tên chủ TK", "Vladimir Vladimirovich Putin"),
+                  infoRow("Giới tính", "Nam"),
+                  infoRow("Ngày sinh", "07-10-1952"),
+                  infoRow("Số CMND/CCCD/HC", "120133456038"),
+                  infoRow("Ngày cấp", "04-04-2016"),
+                  infoRow(
+                    "Nơi cấp",
+                    "Cục Cảnh sát Quản lý Hành chính về Trật tự Xã hội",
+                  ),
+                  infoRow(
+                    "Địa chỉ liên hệ",
+                    "Số 13 Lò Đúc, Phường Thanh Xuân Bắc, Quận Thanh Xuân",
+                  ),
+                  infoRow("Tỉnh/Thành phố", "Hà Nội"),
+                  infoRow("Điện thoại di động", "0969387963"),
+                  infoRow("Email", "putin@gmail.com"),
+                ],
+              ),
+            ),
+            const SizedBox(height: 30),
           ],
         ),
       ),
+    );
+  }
+
+  Widget infoRow(String title, String value, {bool showDivider = true}) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: GoogleFonts.manrope(
+            fontSize: 12,
+            color: Colors.grey,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        const SizedBox(height: 2),
+        Text(
+          value,
+          style: GoogleFonts.manrope(
+            fontSize: 14,
+            color: Colors.white,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        if (showDivider)
+          const Divider(color: Colors.grey, thickness: 0.3, height: 16),
+      ],
     );
   }
 }
